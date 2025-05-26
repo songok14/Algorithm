37b7e683de958df70d2959ab35d80263dea985bb
songok14 <rlathddhr11@gmail.com>
2025-05-26 15:02:06 +0900

    [Bronze II] Title: Hashing, Time: 104 ms, Memory: 14224 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -2,19 +2,30 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main { // 문제 : 알파벳 찾기
+public class Main { 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int num = Integer.parseInt(br.readLine());
+        int L = Integer.parseInt(br.readLine());
         String al = br.readLine();
-        int r = 31;
-        int m = 1234567891;
-        int result = 0;
+        long r = 31;
+        long m = 1234567891;
+        long result = 0;
+        long power_r = 1; // 이 변수는 r^i % m 값을 저장합니다.
 
         for (int i = 0; i < al.length(); i++) {
-            result += (al.charAt(i)-96) * (int)Math.pow(r, i);
+            long char_val = al.charAt(i) - 96; // 'a'는 97이므로, 'a' - 96 = 1 입니다.
+
+            // 현재 항 계산: (char_val * (r^i % m)) % m
+            long term = (char_val * power_r) % m;
+
+            // 결과에 더하기: result = (result + term) % m
+            result = (result + term) % m;
+
+            // 다음 반복을 위해 power_r 업데이트: power_r = (power_r * r) % m
+            // 다음 루프에서는 r^(i+1) % m 값이 됩니다.
+            power_r = (power_r * r) % m;
         }
-        result %= m;
+
         System.out.println(result);
         br.close();
     }
