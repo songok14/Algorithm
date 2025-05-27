a27584c73796be10c80b3070901951f1de9cb330
songok14 <rlathddhr11@gmail.com>
2025-05-27 21:28:26 +0900

    [Bronze I] Title: 이항 계수 1, Time: 124 ms, Memory: 14176 KB -BaekjoonHub

---
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        br.close();
+
+        // 이항계수 : n개 중에 k개 만큼 뽑는 조합의 수
+        // 공식 : n! / (n-k)!k!
+        if(k==0 || n==0 || (n-k) == 0){
+            System.out.println(1);
+        } else {
+            System.out.println(fac(n) / (fac(n - k) * fac(k)));
+        }
+    }
+    public static int fac(int n){
+        return n <= 1 ? n : n * fac(n-1);
+    }
+}
