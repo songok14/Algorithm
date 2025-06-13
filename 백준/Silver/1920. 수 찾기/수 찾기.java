f2f9e634a1d1e7e48152325e5b4be5b128873ff2
songok14 <rlathddhr11@gmail.com>
2025-06-13 11:37:53 +0900

    [Silver IV] Title: 수 찾기, Time: 660 ms, Memory: 46860 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,34 +1,35 @@
 import java.io.*;
-import java.util.StringTokenizer;
-import java.util.TreeSet;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int N = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        TreeSet<Integer> tree = new TreeSet<>();
-        for (int i = 0; i < N; i++) {
-            tree.add(Integer.parseInt(st.nextToken()));
+        int n = Integer.parseInt(br.readLine());
+        String[] nNums = br.readLine().split(" ");
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.parseInt(nNums[i]);
         }
+        Arrays.sort(arr);
 
-        int M = Integer.parseInt(br.readLine());
-        st = new StringTokenizer(br.readLine());
-        br.close();
-
+        int m = Integer.parseInt(br.readLine());
+        String[] mNums = br.readLine().split(" ");
+        
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < M; i++) {
-            if (tree.contains(Integer.parseInt(st.nextToken()))) {
+        for (int i = 0; i < mNums.length; i++) {
+            int index = Arrays.binarySearch(arr, Integer.parseInt(mNums[i]));
+            if (index >= 0) {
                 sb.append(1).append("\n");
             } else {
                 sb.append(0).append("\n");
             }
         }
-
+        
         bw.write(sb.toString());
         bw.flush();
         bw.close();
+        br.close();
     }
 }
