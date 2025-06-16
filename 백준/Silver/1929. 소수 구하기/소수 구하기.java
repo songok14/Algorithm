5170f9d691f406b0b368cd8be4654b51fa8ddd22
songok14 <rlathddhr11@gmail.com>
2025-06-16 15:45:00 +0900

    [Silver III] Title: 소수 구하기, Time: 548 ms, Memory: 22896 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -1,14 +1,15 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
         String[] arr = br.readLine().split(" ");
         int m = Integer.parseInt(arr[0]);
         int n = Integer.parseInt(arr[1]);
 
+        StringBuffer sb = new StringBuffer();
         loop1:
         for (int i = m; i <= n; i++) {
             if(i==1) continue;
@@ -17,7 +18,12 @@ public class Main {
                     continue loop1;
                 }
             }
-            System.out.println(i);
+            sb.append(i).append("\n");
         }
+        
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
