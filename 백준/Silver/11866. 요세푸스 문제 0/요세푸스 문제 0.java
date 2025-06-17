78d7e12ba24993298ae918e3fd495afbe4fce410
songok14 <rlathddhr11@gmail.com>
2025-06-17 09:31:20 +0900

    [Silver IV] Title: 요세푸스 문제 0, Time: 140 ms, Memory: 23340 KB -BaekjoonHub

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
@@ -6,25 +6,26 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        Queue<Integer> q = new LinkedList<>();
+        StringBuffer sb = new StringBuffer();
 
-        String[] input = br.readLine().split(" ");
-        int testCase = Integer.parseInt(input[0]);
-        int reNum = Integer.parseInt(input[1]);
-
-        for (int i = 1; i <= testCase; i++) {
-            q.add(i);
+        Queue<Integer> myQ = new LinkedList<>();
+        String[] inputs = br.readLine().split(" ");
+        int n = Integer.parseInt(inputs[0]);
+        int k = Integer.parseInt(inputs[1]);
+        // 큐에 데이터 입력
+        for (int i = 1; i <= n; i++) {
+            myQ.add(i);
         }
 
-        StringBuilder sb = new StringBuilder();
+        // K 번째 제거
         sb.append("<");
-        while(q.size()>1){
-            for(int i=0; i<reNum-1; i++){
-                q.add(q.poll());
+        while (myQ.size() > 1) {
+            for (int i = 0; i < k - 1; i++) {
+                myQ.add(myQ.poll());
             }
-            sb.append(q.poll()).append(", ");
+            sb.append(myQ.poll()).append(", ");
         }
-        sb.append(q.poll()).append(">");
+        sb.append(myQ.poll()).append(">");
 
         bw.write(sb.toString());
         bw.flush();
