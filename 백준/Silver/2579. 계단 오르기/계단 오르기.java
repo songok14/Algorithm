239959f87eb48b18aac835c987dbbc7bac0e7362
songok14 <rlathddhr11@gmail.com>
2025-07-01 16:33:45 +0900

    [Silver III] Title: 계단 오르기, Time: 104 ms, Memory: 14080 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -1,47 +1,30 @@
-import java.io.*;
-import java.util.Arrays;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuffer sb = new StringBuffer();
 
-        int n = Integer.parseInt(br.readLine()); // 계단의 개수
+        int testCase = Integer.parseInt(br.readLine());
+        int[] dp = new int[testCase + 1];
+        int[] score = new int[testCase + 1];
 
-        int[] scores = new int[n + 1];
-        for (int i = 1; i <= n; i++) {
-            scores[i] = Integer.parseInt(br.readLine());
+        for (int i = 1; i <= testCase; i++) {
+            score[i] = Integer.parseInt(br.readLine());
         }
 
-        if (n == 0) {
-            sb.append(0);
-        } else if (n == 1) {
-            sb.append(scores[1]);
-        } else if (n == 2) { // n=2일 때 초기값 처리
-            sb.append(scores[1] + scores[2]);
-        } else {
-            int[] dp = new int[n + 1];
+        dp[0] = 0;
+        if (testCase >= 1) dp[1] = score[1];
+        if (testCase >= 2) dp[2] = score[1] + score[2];
 
-            // 초기값 설정
-            dp[1] = scores[1];
-            dp[2] = scores[1] + scores[2]; // 10 + 10 = 20
-
-            // 점화식
-            for (int i = 3; i <= n; i++) {
-                // i번째 계단을 밟는 두 가지 방법 중 최댓값 선택
-                // 1. (i-3 -> i-1) + (i-1 -> i) : scores[i] + scores[i-1] + dp[i-3]
-                //    (이전 계단 i-1을 밟고 온 경우. 이때 i-1은 i-3에서 2칸 점프로 왔어야 함)
-                // 2. (i-2 -> i) : scores[i] + dp[i-2]
-                //    (두 칸 전 계단 i-2를 밟고 온 경우)
-                dp[i] = Math.max(dp[i-2], dp[i-3] + scores[i-1]) + scores[i];
-            }
-            sb.append(dp[n]);
+        for (int i = 3; i <= testCase; i++) {
+            int x = dp[i - 3] + score[i - 1];
+            int y = dp[i - 2];
+            dp[i] = Math.max(x, y) + score[i];
         }
+        System.out.println(dp[testCase]);
 
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
         br.close();
     }
 }
