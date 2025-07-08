c6e439da05a6b54f2cd52e0390a8f209d9a87dff
songok14 <rlathddhr11@gmail.com>
2025-07-08 22:46:42 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 784 ms, Memory: 61060 KB -BaekjoonHub

--- 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.java
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.java
@@ -10,67 +10,59 @@ public class Main {
         String[] testCase = br.readLine().split(" ");
         int n = Integer.parseInt(testCase[0]);
         int m = Integer.parseInt(testCase[1]);
-        
         int[][] map = new int[n][m];
         int[][] result = new int[n][m];
-        boolean[][] visited = new boolean[n][m];
-        
-        int[] dx = {1, -1, 0, 0}; 
+        int[] dx = {1, -1, 0, 0};
         int[] dy = {0, 0, 1, -1};
 
         int startX = 0;
         int startY = 0;
-
         for (int i = 0; i < n; i++) {
             String[] inputs = br.readLine().split(" ");
             for (int j = 0; j < m; j++) {
                 map[i][j] = Integer.parseInt(inputs[j]);
-                
-                if (map[i][j] == 1) {
-                    result[i][j] = -1; 
-                } else if (map[i][j] == 2) {
+                if (map[i][j] == 2) {
                     startX = i;
                     startY = j;
                     result[i][j] = 0;
+                } else if (map[i][j] == 1) {
+                    result[i][j] = -1;
                 } else {
                     result[i][j] = 0;
                 }
             }
         }
 
-        Queue<int[]> queue = new LinkedList<>();
-        
-        queue.add(new int[]{startX, startY, 0});
+        Queue<int[]> myQ = new LinkedList<>();
+        boolean[][] visited = new boolean[n][m];
+
+        myQ.add(new int[]{startX, startY, 0});
         visited[startX][startY] = true;
 
-        while (!queue.isEmpty()) {
-            int[] current = queue.poll();
-            int x = current[0];
-            int y = current[1];
-            int distance = current[2];
+        while (!myQ.isEmpty()) {
+            int[] temp = myQ.poll();
+            int x = temp[0];
+            int y = temp[1];
 
             for (int k = 0; k < 4; k++) {
                 int nx = x + dx[k];
                 int ny = y + dy[k];
-
                 if (nx >= 0 && ny >= 0 && nx < n && ny < m && !visited[nx][ny]) {
                     if (map[nx][ny] == 0) {
                         visited[nx][ny] = true;
-                        continue; 
-                    } 
-                    else if (map[nx][ny] == 1) {
+                        continue;
+                    } else if (map[nx][ny] == 1) {
+                        result[nx][ny] = temp[2] + 1;
+                        myQ.add(new int[]{nx, ny, temp[2] + 1});
                         visited[nx][ny] = true;
-                        result[nx][ny] = distance + 1;
-                        queue.add(new int[]{nx, ny, distance + 1});
                     }
                 }
             }
         }
 
-        // 결과 출력
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                sb.append(result[i][j]).append(" ");
+        for (int[] arr : result) {
+            for (int a : arr) {
+                sb.append(a).append(" ");
             }
             sb.append("\n");
         }
@@ -79,5 +71,6 @@ public class Main {
         bw.write(sb.toString().trim());
         bw.flush();
         bw.close();
+
     }
 }
