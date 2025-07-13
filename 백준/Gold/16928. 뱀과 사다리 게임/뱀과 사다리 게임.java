dd0db42fd2d984c0c51cfb87abf91a861c800eec
songok14 <rlathddhr11@gmail.com>
2025-07-13 17:11:55 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 112 ms, Memory: 14948 KB -BaekjoonHub

--- 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.java
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.java
@@ -8,26 +8,27 @@ public class Main {
         String[] testCase = br.readLine().split(" ");
         int n = Integer.parseInt(testCase[0]);
         int m = Integer.parseInt(testCase[1]);
-        int[][] ladder = new int[n][2];
-        int[][] snake = new int[m][2];
+        int[] map = new int[101];
+
+        for (int i=0; i<map.length; i++){
+            map[i] = i;
+        }
 
         // 사다리 정보
         for (int i = 0; i < n; i++) {
             String[] inputs = br.readLine().split(" ");
-            ladder[i][0] = Integer.parseInt(inputs[0]);
-            ladder[i][1] = Integer.parseInt(inputs[1]);
+            map[Integer.parseInt(inputs[0])] = Integer.parseInt(inputs[1]);
         }
 
         // 뱀 정보
         for (int i = 0; i < m; i++) {
             String[] inputs = br.readLine().split(" ");
-            snake[i][0] = Integer.parseInt(inputs[0]);
-            snake[i][1] = Integer.parseInt(inputs[1]);
+            map[Integer.parseInt(inputs[0])] = Integer.parseInt(inputs[1]);
         }
 
         int[] dice = {1, 2, 3, 4, 5, 6};
         Queue<int[]> myQ = new LinkedList<>();
-        boolean[] visited = new boolean[101];
+        boolean[] visited = new boolean[101];   // 총 100칸
 
         int count = 0;
         myQ.add(new int[]{1, count});    // {위치, 카운트}
@@ -36,6 +37,7 @@ public class Main {
         while (!myQ.isEmpty()) {
             int[] temp = myQ.poll();
 
+            // 100에 도착하면 출력 후 종료
             if (temp[0] == 100) {
                 System.out.println(temp[1]);
                 break;
@@ -44,18 +46,9 @@ public class Main {
             for (int i = 0; i < 6; i++) {
                 int nx = temp[0] + dice[i];
 
+                // 이미 방문한 칸을 다시세는 건 의미가 없으니 제외, 무한루프 방지
                 if (nx <= 100 && !visited[nx]) {
-                    for (int j = 0; j < ladder.length; j++) {
-                        if (nx == ladder[j][0]) {
-                            nx = ladder[j][1];
-                        }
-                    }
-
-                    for (int j = 0; j < snake.length; j++) {
-                        if (nx == snake[j][0]) {
-                            nx = snake[j][1];
-                        }
-                    }
+                    nx = map[nx];
 
                     myQ.add(new int[]{nx, temp[1] + 1});
                     visited[nx] = true;
