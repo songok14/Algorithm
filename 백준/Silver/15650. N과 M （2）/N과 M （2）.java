79dcbf74eb9fe30d1d4e1724995a29f5e911b0bb
songok14 <rlathddhr11@gmail.com>
2025-09-24 22:10:03 +0900

    [Silver III] Title: N과 M (2), Time: 104 ms, Memory: 14316 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -1,51 +1,53 @@
 import java.io.*;
 import java.util.ArrayList;
-import java.util.Comparator;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuffer sb = new StringBuffer();
+        StringBuilder sb = new StringBuilder();
 
-        String[] inputs = br.readLine().split(" ");
-        int n = Integer.parseInt(inputs[0]);
-        int target = Integer.parseInt(inputs[1]);
+        String[] testCase = br.readLine().split(" ");
+        int n = Integer.parseInt(testCase[0]);
+        int m = Integer.parseInt(testCase[1]);
 
         List<Integer> myList = new ArrayList<>();
         for (int i = 1; i <= n; i++) {
             myList.add(i);
         }
-        myList.sort(Comparator.naturalOrder());
 
-        List<List<Integer>> result = new ArrayList<>();
-        combi(myList, new ArrayList<>(), result, target, 0);
+        Set<List<Integer>> result = new LinkedHashSet<>();
+        combi(myList, new ArrayList<>(), result, m, 0);
 
         for (List<Integer> list : result) {
             for (int i : list) {
-                sb.append(i).append(" ");
+                sb.append(i);
+                if (i != list.get(list.size() - 1)){
+                    sb.append(" ");
+                }
             }
             sb.append("\n");
         }
 
+        br.close();
         bw.write(sb.toString());
         bw.flush();
         bw.close();
-        br.close();
     }
 
-    public static void combi(List<Integer> myList, List<Integer> temp, List<List<Integer>> result, int target, int start) {
-        if (temp.size() == target) {
+    public static void combi(List<Integer> myList, List<Integer> temp, Set<List<Integer>> result, int m, int start) {
+        if (temp.size() == m) {
             result.add(new ArrayList<>(temp));
             return;
         }
 
         for (int i = start; i < myList.size(); i++) {
             temp.add(myList.get(i));
-            combi(myList, temp, result, target, i+1);
+            combi(myList, temp, result, m, i + 1);
             temp.remove(temp.size() - 1);
         }
-
     }
 }
