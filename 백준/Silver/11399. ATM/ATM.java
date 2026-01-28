300bd1d86727cd7b5edf537f8728bd6da3a8f410
songok14 <rlathddhr11@gmail.com>
2026-01-28 14:00:24 +0900

    [Silver IV] Title: ATM, Time: 100 ms, Memory: 14664 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.java
+++ 백준/Silver/11399. ATM/ATM.java
@@ -4,33 +4,20 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuffer sb = new StringBuffer();
 
-        int n = Integer.parseInt(br.readLine());
-        String[] strArr = br.readLine().split(" ");
-        int[] numbers = new int[n];
-        for(int i=0; i<n; i++){
-            numbers[i] = Integer.parseInt(strArr[i]);
-        }
+        int N = Integer.parseInt(br.readLine());
+        int[] align = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        Arrays.sort(align);
+
+        int sum = 0;
         int total = 0;
-        Arrays.sort(numbers);
-
-        List<Integer> time = new ArrayList<>();
-        time.add(numbers[0]);
-        for (int i = 1; i < n; i++) {
-            int temp = time.get(i - 1);
-            time.add(numbers[i] + temp);
+        for (int i = 0; i < N; i++) {
+            sum = sum + align[i];
+            total += sum;
         }
 
-        for(int i=0; i<time.size(); i++){
-            total += time.get(i);
-        }
-        sb.append(total);
+        System.out.println(total);
 
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
         br.close();
     }
 }
