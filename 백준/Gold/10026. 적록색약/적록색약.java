c407ba0697e0da25d15be63a8435a57aad8d2b0d
songok14 <rlathddhr11@gmail.com>
2026-04-12 16:17:45 +0900

    [Gold V] Title: 적록색약, Time: 168 ms, Memory: 17420 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.java
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -6,87 +6,79 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
-        int[][] mapA = new int[n][n];
-        int[][] mapB = new int[n][n];
 
+        String[][] map = new String[n][n];
         for (int i = 0; i < n; i++) {
             String[] inputs = br.readLine().split("");
             for (int j = 0; j < n; j++) {
-                if (inputs[j].equals("R")) {
-                    mapA[i][j] = 0;
-                    mapB[i][j] = 0;
-                } else if (inputs[j].equals("G")) {
-                    mapA[i][j] = 1;
-                    mapB[i][j] = 0;
-                } else {
-                    mapA[i][j] = 2;
-                    mapB[i][j] = 1;
-                }
+                map[i][j] = inputs[j];
             }
         }
 
         int[] dx = {1, -1, 0, 0};
         int[] dy = {0, 0, 1, -1};
 
-
-        // 일반인
-        Queue<int[]> myQ = new LinkedList<>();
+        Queue<int[]> myQ = new ArrayDeque<>();
         boolean[][] visited = new boolean[n][n];
         int count = 0;
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-
-                if (!visited[i][j]) {
-                    myQ.add(new int[]{i, j, mapA[i][j]});
-                    visited[i][j] = true;
-                    count++;
-                } else {
-                    continue;
-                }
+                if (visited[i][j]) continue;
+                myQ.offer(new int[]{i, j});
+                visited[i][j] = true;
+                String currentText = map[i][j];
+                count++;
 
                 while (!myQ.isEmpty()) {
-                    int[] temp = myQ.poll();
+                    int[] current = myQ.poll();
+                    int cx = current[0];
+                    int cy = current[1];
 
-                    for (int k = 0; k < 4; k++) {
-                        int nx = temp[0] + dx[k];
-                        int ny = temp[1] + dy[k];
+                    for (int d = 0; d < 4; d++) {
+                        int nx = cx + dx[d];
+                        int ny = cy + dy[d];
 
-                        if (nx>=0 && ny>=0 && nx<n && ny<n && !visited[nx][ny] && mapA[nx][ny] == temp[2]){
-                            myQ.add(new int[]{nx, ny, temp[2]});
-                            visited[nx][ny] = true;
-                        }
+                        if (nx < 0 || ny < 0 || nx >= n || ny >= n) continue;
+                        if (visited[nx][ny] || !map[nx][ny].equals(currentText)) continue;
+                        myQ.offer(new int[]{nx, ny});
+                        visited[nx][ny] = true;
                     }
                 }
             }
         }
-        System.out.print(count+" ");
+        System.out.print(count + " ");
 
-        // 적록색약
         myQ.clear();
         visited = new boolean[n][n];
         count = 0;
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-
-                if (!visited[i][j]) {
-                    myQ.add(new int[]{i, j, mapB[i][j]});
-                    visited[i][j] = true;
-                    count++;
-                } else {
-                    continue;
-                }
+                if (visited[i][j]) continue;
+                myQ.offer(new int[]{i, j});
+                visited[i][j] = true;
+                String currentText = map[i][j];
+                count++;
 
                 while (!myQ.isEmpty()) {
-                    int[] temp = myQ.poll();
+                    int[] current = myQ.poll();
+                    int cx = current[0];
+                    int cy = current[1];
 
-                    for (int k = 0; k < 4; k++) {
-                        int nx = temp[0] + dx[k];
-                        int ny = temp[1] + dy[k];
+                    for (int d = 0; d < 4; d++) {
+                        int nx = cx + dx[d];
+                        int ny = cy + dy[d];
 
-                        if (nx>=0 && ny>=0 && nx<n && ny<n && !visited[nx][ny] && mapB[nx][ny] == temp[2]){
-                            myQ.add(new int[]{nx, ny, temp[2]});
+                        if (nx < 0 || ny < 0 || nx >= n || ny >= n) continue;
+                        if (visited[nx][ny]) continue;
+                        if (currentText.equals("R") || currentText.equals("G")) {
+                            if (map[nx][ny].equals("B")) continue;
+                            myQ.offer(new int[]{nx, ny});
+                            visited[nx][ny] = true;
+                        } else {
+                            if (!map[nx][ny].equals(currentText)) continue;
+                            myQ.offer(new int[]{nx, ny});
                             visited[nx][ny] = true;
                         }
                     }
@@ -96,6 +88,5 @@ public class Main {
 
         System.out.println(count);
 
-        br.close();
     }
 }
